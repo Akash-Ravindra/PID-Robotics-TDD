279cb46e832464e9e513fc5157cf31bf178aad7b
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2022-09-30 01:29:07 -0400

    Comment fixes

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file main.cpp
+ * @author Akash Ravindra (aravind2@umd.edu)
+ * @author Sparsh Jaiswal (sjaiswal@umd.edu)
+ * @brief Runs all the tests described in the test file
+ * @version 0.1
+ * @date 2022-09-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
