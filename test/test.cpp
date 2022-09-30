ef2cc808025a00dde6ccefe411010616b6a467d5
Akash_Ravindra_Laptop <akash.ravindra@gmail.com>
2022-09-30 00:59:22 -0400

    Add test to validate the compute function of the PidController class

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,30 @@
+/**
+ * @file test.cpp
+ * @author Akash Ravindra (aravind2@umd.edu)
+ * @author Sparsh Jaiswal (sjaiswal@umd.edu)
+ * @brief Tests to validate the functionality of the Robot PID class. Test indicate specs desired
+ * @version 0.1
+ * @date 2022-09-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <gtest/gtest.h>
+#include <math.h>
+#include <tuple>
+#include "PidController.hpp"
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+
+
+TEST(PidControllerTest, test_compute) {
+  pid::PidController control(2.0, 0.0, 0.0);
+  EXPECT_NEAR(control.compute(5.0, 7.0), -4.0, 0.0001);
+}
+TEST(PidControllerTest, test_getParams) {
+  pid::PidController control;
+  EXPECT_EQ(control.getControllerParams(), (std::tuple<double, double, double>(0.0,0.0,0.0)));
+}
+TEST(PidControllerTest, test_getError) {
+  pid::PidController control(2.0, 0, 0.1);
+  EXPECT_EQ(control.getError(), 0.0);
 }
