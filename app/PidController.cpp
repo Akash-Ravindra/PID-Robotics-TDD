b35fc8ac60847d0bf3aa6a689b5c0816e65a24d8
adithyagaurav <agaurav51@gmail.com>
2022-10-01 10:29:01 -0400

    [ADD]: Code implementation for PidController compute method

--- app/PidController.cpp
+++ app/PidController.cpp
@@ -10,14 +10,22 @@
  *
  */
 
-#include "PidController.hpp"
+#include "../include/PidController.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <limits>
 
 pid::PidController::PidController(double p, double i, double d)
     : kp{p}, ki{i}, kd{d} {}
 pid::PidController::PidController() {}
 pid::PidController::~PidController() {}
 
-double pid::PidController::compute(double target, double curr) { return 0.0; }
+double pid::PidController::compute(double target, double curr) {
+  error = target - curr;
+  prev_error += error * dt;
+  curr_output = kp*error + ki*prev_error + kd  * error / dt;
+  return curr_output;
+}
 
 std::tuple<double, double, double> pid::PidController::getControllerParams() {
   return std::tuple<double, double, double>(kp, ki, kd);
